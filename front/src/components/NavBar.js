142cd1e7001459e3583daf2e10bdacf800e1faf9
Srisankar2001 <srisank-se20028@stu.kln.ac.lk>
2024-03-01 19:52:26 +0530

    link issue by aki

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar=(props)=>{
             <div className="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul className="navbar-nav mr-auto"  >
                     <li className="nav-item"><Link className="nav-link" to="/home">Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="#">Products</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
                     {isAuthenticated()?<li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>:null}
                     {!isAuthenticated()?<li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>:null}
                     {!isAuthenticated()?<li><Link className="nav-link" to="/login">Login</Link></li>:null}
